d5d14f9ea22204ce65452e2bf970a5923c341549
xiranou <xiranou@gmail.com>
2015-11-12 15:06:03 -0500

    add watchModel to directive scope; change to watch watchModel instread of attrs

--- src/animateOnModelChange.js
+++ src/animateOnModelChange.js
@@ -2,13 +2,17 @@ angular.module('animateOnModelChange',[])
   .directive('animateOnModelChange', [function() {
     return {
       restrict: 'A',
+      scope: {
+        watchModel: "="
+      },
       link: function (scope, element, attrs) {
-        scope.$watch(attrs.animateOnModelChange, function(nv,ov){
+        scope.$watch('watchModel', function(nv,ov){
           element
             .removeClass('change-animate')
             .addClass('change-animate');
           element.on('webkitAnimationEnd mozAnimationEnd animationend', function(e) {
             e.preventDefault();
+            e.stopImmediatePropagation();
             element.removeClass('change-animate');
           });
         });
